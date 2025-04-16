a65073402ae175347e214901e891ce548cf55664
younghong <dodamm111@gmail.com>
2025-04-16 11:05:09 +0900

    DB에서 CSV 파일로 export

--- make_csv.py
+++ make_csv.py
@@ -5,11 +5,11 @@ import os
 
 # .env 불러오기
 load_dotenv()
-dsn = os.getenv("DB_URL")  # 예: postgresql://user:pw@localhost:5432/dbname
+dsn = os.getenv("DB_URL")
 
 # 연결
 conn = psycopg2.connect(dsn)
 
 # 쿼리 → DataFrame → CSV 저장
 df = pd.read_sql_query("SELECT * FROM bus_data", conn)
-df.to_csv('./datas/bus_data2.csv', index=False)
+df.to_csv('./datas/bus_data.csv', index=False)
